94de341e4639b3319d36527b013ffa0ce7bb661b
Richard Kim <richardjkim7@gmail.com>
2022-08-15 14:27:02 -0500

    loops

--- js/loops.js
+++ js/loops.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // #2:
 
 function showMultiplicatonTable(num) {
@@ -6,3 +8,29 @@ function showMultiplicatonTable(num) {
         console.log(`${num} x ${i} = ${multiply}`);
     }
 }
+
+// #3:
+
+function randomNum(min, max) {
+    for (let i = 1; i < 11; i++) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+}
+
+randomNum(20,200);
+
+
+function tenRandom() {
+    for (let i = 1; i < 11; i++) {
+        let random = randomNum(20,200);
+        if (random % 2 === 0) {
+            console.log(`${random} is even`);
+        } else {
+            console.log(`${random} is odd`);
+        }
+    }
+}
+
+tenRandom();
+
+// #4:
