610e537260448f8cec2517c49735ec6c8d7e6b51
Richard Kim <richardjkim7@gmail.com>
2022-08-15 10:59:11 -0500

    intro to testing

--- intro-to-testing-js-master/code.js
+++ intro-to-testing-js-master/code.js
@@ -3,5 +3,11 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 function sayHello(string) {
-    return "Hello," + " " + string + "!";
+    if (string === undefined) {
+        return "Hello, World!";
+    } else if (string === !String) {
+        return "Hello, World!";
+    } else {
+        return "Hello," + " " + string + "!";
+    }
 }
--- intro-to-testing-js-master/tests.js
+++ intro-to-testing-js-master/tests.js
@@ -33,4 +33,7 @@ describe('sayHello', function() {
     it('should return the string "Hello, Pat!" when executed', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
 })
