156a97f3ccec87cec90de7582941104ff137cfd4
Richard Kim <richardjkim7@gmail.com>
2022-08-17 10:23:58 -0500

    while loops

--- js/while.js
+++ js/while.js
@@ -12,4 +12,17 @@ while (num <= 16) {
 
 //do-while loop
 
+let coneSupply = Math.floor(Math.random() * 50) + 50;
 
+do {
+    let conesToSell = Math.floor(Math.random() * 5) + 1;
+    if (conesToSell > coneSupply) {
+        console.log(`Cannot sell you ${conesToSell} cones I only have ${coneSupply}...`);
+    } else if (conesToSell < coneSupply) {
+        console.log(`${conesToSell} cones sold...`);
+        coneSupply = coneSupply - conesToSell;
+    } else {
+        console.log(`Yay! I sold them all!`);
+        coneSupply = coneSupply - conesToSell;
+    }
+} while (coneSupply > 0);
