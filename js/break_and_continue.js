9c3b8ce89b36a4bc4a62fa7eb6726f06abe816a0
Richard Kim <richardjkim7@gmail.com>
2022-08-17 09:53:47 -0500

    break_and_continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,22 +1,25 @@
 'use strict';
-// let userNumber = parseInt(prompt("Enter a number between 1 and 50:"));
-// while (userNumber < 1 || userNumber > 50) {
-//     if (userNumber > 1 && userNumber < 50) {
-//         break;
-//     } else {
-//         userNumber = prompt("Enter a valid number");
-//     }
-// }
-
-let num = 1;
-while (num < 50) {
-    if (num % 2 === 0) {
-        continue;
-    } else if (num === userNumber) {
-        console.log(`Yikes! Skipping number: ${userNumber}`)
+let userNumber = parseInt(prompt("Enter a number between 1 and 50:"));
+while (userNumber < 1 || userNumber > 50) {
+    if (userNumber > 1 && userNumber < 50) {
+        break;
+    } else {
+        userNumber = prompt("Enter a valid number");
     }
-    console.log(`Here is an odd number: ${num}`);
-    num++;
+}
+
+function userNum(userNumber) {
+    for (let i = 0; i < 50; i++) {
+        if (i % 2 !== 0) {
+            if (i == userNumber) {
+                console.log(`Yikes! Skipping number: ${userNumber}`);
+                continue;
+            }
+        } else {
+            continue;
+        }
+        console.log(`Here is an odd number: ${i}`);
+    }
 }
 
 
