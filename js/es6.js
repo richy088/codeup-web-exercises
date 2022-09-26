6ff158b7b682e47c1fa681cffbba5701b6e19e3e
Richard Kim <richardjkim7@gmail.com>
2022-09-26 11:48:40 -0500

    es6 exercises

--- js/es6.js
+++ js/es6.js
@@ -73,17 +73,31 @@ users.forEach(function(user) {
   const email = user.email;
   const languages = user.languages;
 
+
+  });
+
+
+
   // TODO: rewrite the assignment below to use template strings
   developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
 });
 
+developers.push(`${name} \'s email is' ${email} ${name} knows ${languages.join(', ')}`);
+
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
 // TODO: rewrite the following loop to use a for..of loop
 developers.forEach(function (developer) {
 
+  for (let developer of developers) {
+
+  }
+
   // TODO: rewrite the assignment below to use template strings
   list += '<li>' + developer + '</li>';
 });
+  list += `<li> ${developer} </li>`
+
 list += '</ul>';
+
