38e72b96bdf0adb075711a6f82832a537df9ba8b
Richard Kim <richardjkim7@gmail.com>
2022-09-26 10:51:32 -0500

    es6 exercises

--- js/es6.js
+++ js/es6.js
@@ -39,14 +39,14 @@ const languages = ['html', 'css', 'javascript'];
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name: name,
-  email: email,
-  languages: languages
+  name,
+  email,
+  languages
 });
 
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
 users.forEach(function(user) {
