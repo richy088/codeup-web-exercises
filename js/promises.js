c955a2de1bfd882927bcba87e8bcf39c9460cb6c
Richard Kim <richardjkim7@gmail.com>
2022-10-01 22:21:15 -0500

    promises exercise complete

--- js/promises.js
+++ js/promises.js
@@ -14,3 +14,26 @@ function getUserData (userName) {
 }
 
 getUserData("richy088");
+
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//     console.log('Lottery draw is happening');
+//     setTimeout(function () {
+//         if (Math.random() >= 0.5) {
+//             resolve('You WIN');
+//         } else {
+//             reject(new Error('You lost your money'));
+//         }
+//     }, 2000);
+// });
+//
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+const wait = function(seconds) {
+    return new Promise(function (resolve){
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+wait(1).then(()=>{
+    console.log(`You'll see this after 1 second`);
+    return wait(3);
+}).then(() => console.log(`You'll see this after 3 seconds`));
